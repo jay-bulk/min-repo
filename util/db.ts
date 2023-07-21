fcc3997a597b641e65b2e5fc2d6bca6d1b81d113
jay-bulk <rbulkley@byu.edu>
2023-07-21 12:31:38 -0600

    refactor: cleanup db

--- util/db.ts
+++ util/db.ts
@@ -18,36 +18,13 @@ export class DBProvider {
       connectString: configOptions?.connectString ?? process.env.DB_CONNECT_STRING
     }
   }
-  static async init (): Promise<void> {
+
+  async init(): Promise<void> {
     try {
-     this.db = createConnectionPool()
+      DBProvider.db = createConnectionPool()
     } catch (e) {
       console.error(e)
-      await this.db?.dispose()
+      await DBProvider.db?.dispose()
     }
-  }
-
-  async testConnection (db: ConnectionPool): Promise<Boolean> {
-    try {
-      if (db == null) {
-        throw new Error('No connection pool. Did you run the init() function?')
-      }
-       const result = await db.query(sql`select 1 + 1 as result`)
-      console.log(result)
-      return true
-    } catch (e) {
-      console.error(e)
-      await db?.dispose()
-      return false
-    }
-  }
-
-  async execute<T = unknown>(sql: SQLQuery): Promise<T[]> {
-    if (DBProvider.db == null) {
-      throw new Error('No connection. Did you ruyn the init() function?')
-    }
-    let result = await DBProvider.db.query(sql)
-    return (result ?? [])
   }
 }
-export default DBProvider
