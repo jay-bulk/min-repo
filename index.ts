9c4eec234dae02f2049873af56744d36dba624d6
jay-bulk <rbulkley@byu.edu>
2023-07-21 11:19:49 -0600

    feat: db plugin

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-import fastify from 'fastify'
+import Fastify, {FastifyInstance} from 'fastify'
+import db from "./plugins/db";
+import auth from './plugins/auth'
+export async function app (): Promise<FastifyInstance> {
+  let fastify: FastifyInstance = Fastify({})
+  await fastify.register(db)
+  await fastify.register(auth)
 
-fastify
+  return fastify
+}
+
+
+
+
--- util/db.ts
+++ util/db.ts
@@ -1,9 +1,4 @@
-import {
-  Pool,
-  PoolAttributes,
-} from 'oracledb'
-import { logger } from '../utils'
-import {sql, Connection} from '@databases/pg'
+import createConnectionPool, {sql, ConnectionPool, SQLQuery} from '@databases/pg'
 
 
 interface envOptions {
@@ -14,13 +9,45 @@ interface envOptions {
 
 export class DBProvider {
   private readonly dbConfig: envOptions
-  private readonly providerAttributes?: PoolAttributes | undefined
-  private pool: Pool | undefined
-  constructor (configOptions?: envOptions, poolOptions?: PoolAttributes) {
+  private static db: ConnectionPool | undefined;
+
+  constructor(configOptions?: envOptions) {
     this.dbConfig = {
-      user: configOptions?.user ?? process.env.ORACLEDB_USER,
-      password: configOptions?.password ?? process.env.ORACLEDB_PASSWORD,
-      connectString: configOptions?.connectString ?? process.env.ORACLEDB_CONNECT_STRING
+      user: configOptions?.user ?? process.env.DB_USER,
+      password: configOptions?.password ?? process.env.DB_PASSWORD,
+      connectString: configOptions?.connectString ?? process.env.DB_CONNECT_STRING
     }
-    // this.providerAttributes = poolOptions //TODO implement other options as we find we need them (poolMin/Max, etc.)
   }
+  static async init (): Promise<void> {
+    try {
+     this.db = createConnectionPool()
+    } catch (e) {
+      console.error(e)
+      await this.db?.dispose()
+    }
+  }
+
+  async testConnection (db: ConnectionPool): Promise<Boolean> {
+    try {
+      if (db == null) {
+        throw new Error('No connection pool. Did you run the init() function?')
+      }
+       const result = await db.query(sql`select 1 + 1 as result`)
+      console.log(result)
+      return true
+    } catch (e) {
+      console.error(e)
+      await db?.dispose()
+      return false
+    }
+  }
+
+  async execute<T = unknown>(sql: SQLQuery): Promise<T[]> {
+    if (DBProvider.db == null) {
+      throw new Error('No connection. Did you ruyn the init() function?')
+    }
+    let result = await DBProvider.db.query(sql)
+    return (result ?? [])
+  }
+}
+export default DBProvider
